e54361966d078032d43496d897d3c553ee33b3d9
Abel Koech <koechabel161@gmail.com>
2024-08-27 13:16:34 +0300

    comprehensions

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    #pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print('Happy New Year!')
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    #pass
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    #pass
+    results= [
+         'FizzBuzz' if num % 3 == 0 and num % 5 == 0
+          else 'Fizz' if num % 3 == 0
+          else 'Buzz' if num % 5 == 0
+          else num
+          for num in range(1, 101)
+    ]
+    for result in results:
+      print(result)
